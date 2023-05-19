228628ca4408fe48c6291931b0bdbea206d669bb
Juluisuwu <medranoreyesjc@gmail.com>
2023-05-18 23:33:08 -0600

    a ver este

---
+++ switch.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    char* opc;
+    while(1){
+        system("clear");
+        printf("\nMenu Princial de Biblioteca");
+        printf("a)Registro de usuario\nb)Registra un libro\nc)Prestamo de libro\nd)Entrega del libro\ne)Busqueda de libro\nf)Buscar usuario\ng)Salir");
+        
+        if (*opc == 'S') break;
+    }
+}
